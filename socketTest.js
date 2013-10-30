478b0d059d2138a9500124f2a842aa816608572e
kimhaeyeon <data_mwhy@naver.com>
2013-10-30 14:12:17 +0900

    [modify] 방 정보 개설
    방 개설

--- socketTest.js
+++ socketTest.js
@@ -3,7 +3,7 @@ var app = require('http').createServer(handler)
   , fs = require('fs');
   var port = process.env.PORT || 5000;
 
-app.listen(port);
+/*app.listen(port);
 
 function handler (req, res) {
   fs.readFile( 'htmlPage.html',
@@ -39,4 +39,35 @@ io.sockets.on('connection', function (socket) {
 		});
 	});
 
+});*/
+var server = require('http').createServer();
+var io = require('socket.io').listen(server);
+
+// 서버를 실행합니다.
+server.listen(port, function () {
+    console.log('Server Running at http://127.0.0.1:52273');
+});
+
+// 웹 서버 이벤트를 연결합니다.
+server.on('request', function (request, response) {
+    // HTMLPage.htm 파일을 읽습니다.
+    fs.readFile('htmlPage.htm', function (error, data) {
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(data);
+    });
+});
+
+// 소켓 서버 이벤트를 연결합니다.
+io.sockets.on('connection', function (socket) {
+    // join 이벤트
+    socket.on('join', function (data) {
+        socket.join(data);
+        socket.set('room', data);
+    });
+    // message 이벤트
+    socket.on('message', function (data) {
+        socket.get('room', function (error, room) {
+            io.sockets.in(room).emit('message', data);
+        });
+    });
 });
