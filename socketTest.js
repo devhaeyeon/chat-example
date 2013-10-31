18cd8c7f25a98136552daa5b3aa21a49a6802a45
kimhaeyeon <data_mwhy@naver.com>
2013-10-31 10:48:48 +0900

    [modify] 입장 메세지 출력하는 곳 수정
    입장 메세지 출력하는 곳 수정

--- socketTest.js
+++ socketTest.js
@@ -27,16 +27,14 @@ io.configure(function () {
 
 io.sockets.on('connection', function (socket) {
 	socket.on('systemIn',function(data){
-		   socket.get('room', function (error, room) {
-				if(data.name)
-				{
-					//최초 입장시 아이디/소켓코드 저장
-					nicklist[data.name] = socket.nickname = data.name;
-					nickidlist[data.name] = socket.id;
-					io.sockets.in(room).emit('systemIn',data);
-					io.sockets.in(room).emit('systemList',nicklist);
-				}
-		});
+		if(data.name)
+		{
+			//최초 입장시 아이디/소켓코드 저장
+			nicklist[data.name] = socket.nickname = data.name;
+			nickidlist[data.name] = socket.id;
+			io.sockets.emit('systemIn',data);
+			io.sockets.emit('systemList',nicklist);
+		}
 	});
 
     // join 이벤트
@@ -63,13 +61,11 @@ io.sockets.on('connection', function (socket) {
 
 	//퇴장 처리
 	socket.on('disconnect',function(){
-        socket.get('room', function (error, room) {
 		if(socket.nickname){
 			socket.broadcast.emit('systemOut',{name:socket.nickname});
 			delete nicklist[socket.nickname];
-			io.sockets.in(room).emit('systemList',nicklist);
-		}		
-		});
+			io.sockets.emit('systemList',nicklist);
+		}
 	});
 
 });
